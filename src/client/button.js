c50cda114d26c7815658df7bb582b1ce10336214
Sean Ho <seansinflipflops@gmail.com>
2017-03-11 17:37:44 -0500

    fixed merge conflicts

--- src/client/button.js
+++ src/client/button.js
@@ -2,10 +2,13 @@ var dislike = document.getElementById("poop");
 var like = document.getElementById("pound");
 var dislike2 = document.getElementById("poop2");
 var like2 = document.getElementById("pound2");
+var xhr = new XMLHttpRequest();
+
 var count = 0;
 
 like.addEventListener("click", function(e) {
-  addUp();
+  addUp()
+
 })
 
 dislike.addEventListener("click", function() {
@@ -22,20 +25,81 @@ dislike2.addEventListener("click", function() {
 
 function addUp() {
     var counter = document.getElementById("counter");
-    if (counter) counter.innerHTML = ++count;
+    if (counter) {
+      counter.innerHTML = ++count;
+      xhr.open('POST',"/add", true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var json = JSON.parse(xhr.responseText);
+          console.log(json)
+        }
+      }
+      var data = JSON.stringify({
+        count: parseInt(counter.innerHTML)
+      });
+      xhr.send(data);
+    }
 };
 
 function minusDown() {
     var counter = document.getElementById("counter");
-    if (counter) counter.innerHTML = --count;
+    if (counter) {
+      counter.innerHTML = --count;
+      xhr.open('POST',"/minus", true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var json = JSON.parse(xhr.responseText);
+          console.log(json)
+        }
+      }
+      var data = JSON.stringify({
+        count: parseInt(counter.innerHTML)
+      });
+      xhr.send(data);
+    }
 };
 
 function addUp2() {
     var counter = document.getElementById("counter2");
-    if (counter) counter.innerHTML = ++count;
+    if (counter) {
+      counter.innerHTML = ++count;
+      xhr.open('POST',"/add2", true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var json = JSON.parse(xhr.responseText);
+          console.log(json)
+        }
+      }
+      var data = JSON.stringify({
+        count2: parseInt(counter.innerHTML)
+      });
+      xhr.send(data);
+    }
+
 };
 
 function minusDown2() {
     var counter = document.getElementById("counter2");
-    if (counter) counter.innerHTML = --count;
+    if (counter) {
+      counter.innerHTML = --count;
+      xhr.open('POST',"/add2", true);
+      xhr.setRequestHeader("Content-type", "application/json");
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var json = JSON.parse(xhr.responseText);
+          console.log(json)
+        }
+      }
+      var data = JSON.stringify({
+        count2: parseInt(counter.innerHTML)
+      });
+      xhr.send(data);
+    }
 };
+
+window.onLoad(
+
+)
