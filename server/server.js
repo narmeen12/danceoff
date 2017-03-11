d783d56c6ceade1f99606ed37db7fe896905f12e
Sean Ho <seansinflipflops@gmail.com>
2017-03-11 13:50:30 -0500

    videos rendering

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config()
+// console.log(process.env)
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
 const app = express();
 
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 app.use(express.static(path.join(__dirname, '/../src/')));
 app.use(express.static(path.join(__dirname, '/../node_modules')));
 app.use(bodyParser.json());
-
 
 app.listen(port, (err) => {
   if(err) {
